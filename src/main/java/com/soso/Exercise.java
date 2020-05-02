92c7a3584aa7f302a538a94be807d2fc2197c9f0
shoushou10 <317874333@qq.com>
2020-05-02 15:05:16 +0800

    commit Exercise

---
+++ src/main/java/com/soso/Exercise.java
@@ -0,0 +1,31 @@
+package com.soso;
+
+public class Exercise {
+    public static void main(String[] args){
+//        int x = 4;
+//        int y = 1;
+//        if(x==y){
+//            System.out.println("equal");
+//        }else{
+//            System.out.println("not equal");
+//        }
+
+//        StringBuffer str1 = new StringBuffer("Runoob");
+//        System.out.println(str1.charAt(0));
+//        System.out.println(str1.delete(1,3));
+
+//        float f = 1f;
+//        System.out.println(Float.SIZE);
+
+//        System.out.println(Math.round(3.7));
+
+//        int i = (int)6.7;
+
+        String str = "runoob";
+        str.toUpperCase();
+        System.out.println(str.length());
+        String str1 = "oob";
+        System.out.println(str.indexOf(str1,4));
+
+    }
+}
